ecb3e9430e7e5f88488495ef6a28ad630462e473
Pradeep-G-A <113411971+Pradeep-G-A@users.noreply.github.com>
2023-06-03 11:34:09 +0530

    Add files via upload
    Day 5

---
+++ Task4.java
@@ -0,0 +1,25 @@
+package com.Day5.DAY_5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Task4 {
+	public static void main(String[] args) throws InterruptedException
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver dr=new ChromeDriver();
+		dr.get("https://j2store.net/free/");
+		dr.manage().window().maximize();
+		dr.findElement(By.xpath("//*[@id=\'Mod112\']/div/div/ul/li[1]/h4/a")).click();
+		dr.navigate().back();
+		dr.navigate().forward();
+		dr.navigate().refresh();
+		Thread.sleep(3000);
+		dr.quit();
+
+	}
+
+}
